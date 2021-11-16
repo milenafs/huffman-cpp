95f6c14ee67aba5ab2facc30f7b01fc28efdc5fc
Milena Furuta Shishito <milenafurutashishito@gmail.com>
2021-11-16 12:06:38 -0300

    classes .h criadas

---
+++ Teste/Teste/ListaOrdenada.h
@@ -0,0 +1,14 @@
+#ifndef ListaOrdenada
+#define ListaOrdenada
+class ListaOrdenada
+{
+	private:
+		static int Numerador;
+		static unsigned int Denominador;
+	public:
+		static void  AssumaValor(int, unsigned int);
+		static char* NaFormaDeString();
+		static void  SomeSeCom(int);hjjy
+		static void  SubtraiaDeSi(int);
+};
+#endif
---
+++ Teste/Teste/NodoDeListaLigada.h
@@ -0,0 +1,15 @@
+#ifndef NodoDeLista
+#define NodoDeLista
+class NodoDeLista
+{
+	private:
+		static int Numerador; 
+		static unsigned int Denominador; 
+	public:
+		static void  AssumaValor(int, unsigned int); 
+		static char* NaFormaDeString(); 
+		static void  SomeSeCom(int); 
+		static void  SubtraiaDeSi(int);
+};
+#endif
+
